28eaeed818d0f1e31eda7fe9394303898521bf93
Steven <43386755+jestmz@users.noreply.github.com>
2021-05-13 17:38:49 -0600

    Add files via upload

--- ProyectoProgra/src/java/org/dao/DaoModulo.java
+++ ProyectoProgra/src/java/org/dao/DaoModulo.java
@@ -37,8 +37,8 @@ public class DaoModulo implements CrudModulo{
                 mod.setACTIVO(rs.getInt("ACTIVO"));
                 mod.setUSUARIO_CREA(rs.getString("USUARIO_CREA"));
                 mod.setUSUARIO_MOD(rs.getString("USUARIO_MOD"));
-                mod.setFECHA_CREA(rs.getDate("FECHA_CREA"));
-                mod.setFECHA_MOD(rs.getDate("FECHA_MOD"));
+                mod.setFECHA_CREA(rs.getString("FECHA_CREA"));
+                mod.setFECHA_MOD(rs.getString("FECHA_MOD"));
                                 
                 lstModulo.add(mod);
             }
@@ -62,10 +62,8 @@ public class DaoModulo implements CrudModulo{
     @Override
     public boolean insertar(MODULO modulo) {
         strSql = "INSERT INTO MODULO (ID_MODULO, NOMBRE, DESCRIPCION, PATH, NIVEL, ORDEN, ID_MODULO_PADRE, ACTIVO, "
-                + "USUARIO_CREA, USUARIO_MOD, FECHA_CREA, FECHA_MOD) "
-                + "SELECT  (SELECT ISNULL(MAX(ID_PEDIDO),0) + 1 FROM PEDIDO), " +                 
-                      
-                 "" + modulo.getID_MODULO() + ", " +
+                + "USUARIO_CREA, USUARIO_MOD, FECHA_CREA, FECHA_MOD) VALUES("
+                + "SELECT  (SELECT ISNULL(MAX(ID_MODULO),0) + 1 FROM MODULO), " +
                 "'" + modulo.getNOMBRE()+ "', " +
                 "'" + modulo.getDESCRIPCION()+ "', " +
                 "'" + modulo.getPATH()+ "', " +
@@ -76,8 +74,7 @@ public class DaoModulo implements CrudModulo{
                 "'" + modulo.getUSUARIO_CREA()+ "', " +
                 "'" + modulo.getUSUARIO_MOD()+ "', " +
                 "'" + modulo.getFECHA_CREA()+ "', " +
-                "'" + modulo.getFECHA_MOD()+ "', " +
-                "WHERE NOT EXISTS(SELECT 1 FROM MODULO WHERE ID_MODULO = " + modulo.getID_MODULO() + ");";
+                "'" + modulo.getFECHA_MOD()+ "' )";
         
         try {
             //se abre una conexión hacia la BD
@@ -99,7 +96,7 @@ public class DaoModulo implements CrudModulo{
 
     @Override
     public boolean modificar(MODULO modulo) {
-        strSql = "UPDATE  CLIENTE SET NOMBRE='" +modulo.getNOMBRE()+ "', "+
+        strSql = "UPDATE  CLIENTE SET NOMBRE = '" +modulo.getNOMBRE()+ "', "+
                  "DESCRIPCION = '" + modulo.getDESCRIPCION()+ "', "+
                  "PATH = '" + modulo.getPATH()+ "' , " +
                  "NIVEL = " + modulo.getNIVEL()+ ", " +
@@ -109,7 +106,7 @@ public class DaoModulo implements CrudModulo{
                  "USUARIO_CREA = '" + modulo.getUSUARIO_CREA()+ "', "+
                  "USUARIO_MOD = '" + modulo.getUSUARIO_MOD()+ "', "+
                  "FECHA_CREA = '" + modulo.getFECHA_CREA()+ "', "+
-                 "FECHA_MOD = '" + modulo.getFECHA_MOD()+ "', "+
+                 "FECHA_MOD = '" + modulo.getDESCRIPCION()+ "', "+
                  "WHERE ID_MODULO = " + modulo.getID_MODULO()+ "";
         
         try {
