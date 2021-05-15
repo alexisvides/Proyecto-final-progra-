43bdda932c18a21fbe37ffa4283d33a289621ab5
Steven <43386755+jestmz@users.noreply.github.com>
2021-05-14 22:15:24 -0600

    Add files via upload

--- ProyectoProgra/src/java/org/controller/ControllerRol.java
+++ ProyectoProgra/src/java/org/controller/ControllerRol.java
@@ -7,28 +7,21 @@ package org.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.dao.DaoRol;
+import org.models.ROL;
 
-/**
- *
- * @author vides
- */
 @WebServlet(name = "ControllerRol", urlPatterns = {"/ControllerRol"})
 public class ControllerRol extends HttpServlet {
+    
+    String ingreso = "index.jsp", vista="rolConsulta.jsp";
+    String modificar = "";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -46,43 +39,60 @@ public class ControllerRol extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String acceso = "";
+        String action = request.getParameter("accion");
+
+        ROL rol = new ROL();
+        DaoRol daoRol = new DaoRol();
+
+        switch (action) {
+            case "vista":
+                acceso = vista;
+                break;
+            case "eliminar":
+                rol = new ROL();
+                rol.setID_ROL(Integer.parseInt(request.getParameter("ID_ROL")));
+                daoRol.eliminar(rol);
+                acceso = vista;
+                break;
+            case "modificar":
+                rol = new ROL();
+                rol.setNOMBRE(request.getParameter("NOMBRE"));
+                rol.setDESCRIPCION(request.getParameter("DESCRIPCION"));
+                rol.setUSUARIO_CREA(request.getParameter("USUARIO_CREA"));
+                rol.setUSUARIO_MOD(request.getParameter("USUARIO_MOD"));
+                
+                daoRol.modificar(rol);
+                acceso = ingreso;
+                break;
+            case "create":
+                rol = new ROL();
+                rol.setNOMBRE(request.getParameter("NOMBRE"));
+                rol.setDESCRIPCION(request.getParameter("DESCRIPCION"));
+                rol.setUSUARIO_CREA(request.getParameter("USUARIO_CREA"));
+                rol.setUSUARIO_MOD(request.getParameter("USUARIO_MOD"));
+                
+                daoRol.modificar(rol);
+                acceso = ingreso;
+                break;
+
+        }
+        RequestDispatcher vista = request.getRequestDispatcher(acceso); //invoca de modo directo un recurso web
+        vista.forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
